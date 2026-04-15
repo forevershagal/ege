46e835996cd5f90e19a0c3e095e9fe2205a7e920
forevershagal <07ramilshagaliev@gmail.com>
2026-04-15 18:58:20 +0300

    new done tasks

--- forExample.py
+++ forExample.py
@@ -1,41 +1,7 @@
-from math import dist
-
-def dbscan(a, r):
-    cl = []
-    while a:
-        cl.append([a.pop(0)])
-        for i in cl[-1]:
-            for j in a[:]:
-                if dist(i, j) < r:
-                    cl[-1].append(j)
-                    a.remove(j)
-    return cl
-
-f = open('/Users/shagal/Desktop/shagalievv/Школково/Информатика/st_27_0303/27_B.txt')
-a = [list(map(float, i.replace(';', ',').split())) for i in f]
-r = 6
-m = 20
-pairs = [(0, 1), (0, 2), (1, 2)]
-diams = []
-st_diams = []
-md_stars = []
-point = (2, 2)
-
-clusters = dbscan(a, r)
-clusters = [i for i in clusters if len(i) > 1]
-
-for i, j in pairs:
-    md = 0
-    for star1 in clusters[i]:
-        for star2 in clusters[j]:
-            if dist(star1, star2) > md:
-                md = dist(star1, star2)
-                md_stars = (star1, star2)
-    diams.append(md)
-    st_diams.append(md_stars[0])
-    st_diams.append(md_stars[1])
-
-q1 = sum(diams)
-q2 = max([dist(p, point) for p in st_diams])
-print(int(q1*100), int(q2*100))
-
+x = 8 ** 190 + 8 ** 100 - 64 ** 3
+s = ''
+while x > 0:
+    s = str(x%8) + s
+    x //= 8
+print(s)
+print(s.count('0'))
